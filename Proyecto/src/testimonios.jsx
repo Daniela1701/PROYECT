a664f7a5c03d39c1cabe8e4f0621c904de42e91e
Daniela1701 <i202409920@cibertec.edu.pe>
2024-08-25 05:15:35 +0000

    que salgaaa

--- Proyecto/src/testimonios.jsx
+++ Proyecto/src/testimonios.jsx
@@ -1,10 +1,29 @@
-import testimonios_titulo from "./componentes/testimonios_titulo"
+import user1 from './componentes/imagenes/t_user_1.jpg'
 
-function testimonios(){
-    return(
-      <>
-      <testimonios_titulo/>
-      </>
-    );
+function testimonios() {
+    return (
+    <>
+        <div className='section'>
+         <h1>BLABLABLA</h1>
+            <div className="figure">
+                <figcaption> @Sonne_peruvian24</figcaption>
+                   <div class="iconos">
+                       <img src={user1} />
+                   </div>     
+                    <p className="texto">
+                     "Tuve un viaje maravilloso a Trujillo. Desde la asesoría 
+                      inicial hasta el regreso a casa, todo fue perfecto. Me
+                      proporcionaron una guía muy útil con información sobre 
+                      cultura, transporte y lugares imprescindibles. Sin duda, 
+                      volveré a contar con ellos para un futuro viaje con mi 
+                      enamorada."
+                    </p>
+                    <p className="opinion"><strong>Excelente trabajo</strong></p>
+            </div>
+        </div>
+
+    </>
+    )
   }
-  export default testimonios
+  
+  export default testimonios;

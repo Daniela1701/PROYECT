209f1ecc2e2a8f845f0abe86b3dcd19cf7ae9a7d
Daniela1701 <i202409920@cibertec.edu.pe>
2024-08-25 04:24:24 +0000

    testimonios

---
+++ Proyecto/src/testimonios.jsx
@@ -0,0 +1,12 @@
+import testimonios_titulo from './componentes/testimonios_titulo'
+import testimonios_comentarios from './componentes/testimonios_comentarios'
+
+function testimonios(){
+    return(
+      <>
+      <testimonios_titulo/>
+      <testimonios_comentarios/>
+      </>
+    );
+  }
+  export default testimonios;
